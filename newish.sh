350ab7f1956c28bfa7b43687e214893900132f19
John Horton <john.joseph.horton@gmail.com>
2020-11-27 10:04:10 -0500

    Finds newish lines in a version controlled file

---
+++ newish.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+if [ $2 == '' ]
+then
+    maxage=1
+else
+    maxage=$2
+fi
+git blame -c --date=raw $1 | sed 's/%//g' | awk 'BEGIN {} 
+{
+    if (NF > 5) {
+	age = (systime() - $4)/(24 * 3600);
+	if (age < maxage){
+	    printf age; 
+	    for (i = 6; i <= NF; i++){
+		printf FS$i
+	    };
+	    printf "\n"
+	}
+    }
+}
+END {}' maxage=$maxage 
+
+
