9a73ebba902a723852c46abc7f4386539eaf9339
John Horton <johnhorton@cloud.upwork.com>
2020-12-05 09:38:14 -0500

    Takes snapshots and places file in the directoy called from, storing images sequentially

---
+++ snapshot.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+# Takes any file, gets the current time and moves it to a snapshot folder
+
+ts=$(/bin/date "+%Y-%m-%d---%H-%M-%S")
+newname=$(echo $1 | sed "s/\./\_$ts\./g") # note double quotes to get string interpolation 
+
+mkdir -p $HOME/snapshots 
+cp $1 $HOME/snapshots/$newname
