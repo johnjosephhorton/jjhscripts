2e0ae731a8712cd99b282f0eb60ae329dc41c9d8
John Horton <john.joseph.horton@gmail.com>
2020-12-05 14:04:52 -0500

    Gave up on awk; using the far superior csv tools package

--- csvdeets.sh
+++ csvdeets.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 # Takes a CSV file as input and gives some statistics about each column.
-# Depends on datamash
+# Depends on datamash and csvtool being installed. 
 
 numrows=$(cat $1 | wc -l)
 printf "Number of Rows: $numrows\n"
@@ -12,46 +12,36 @@ printf "Number of Distinct Rows:$numdistinctrows\n"
 headers=$(awk -F "\"*,\"*" -v j=$i 'NR==1{print $j}' $1)
 echo $headers | wc -l
 
-# -v 
-
 numfields=$(awk -F "\"*,\"*" -v j=$i 'NR==1{print NF}' $1)
 
 for i in $(seq 1 $numfields);
 do
     printf "\n"
     printf "##########################################################\n"
-    printf "Column Name:"
-    awk -F "\"*,\"*" -v j=$i 'NR==1{print $j}' $1
-    #tmpfile=$(mktemp /tmp/abc-script.XXXXXX)
-    awk -v FPAT='[^,]*|"[^"]+"' -v j=$i  'NR>1{print $j}' $1 > $tmpfile
-    printf "Number of unique values:"
-    cat $tmpfile | sort | uniq | wc -l
-    #numValues=$(awk -F "\"*,\"*" -v j=$i  'NR>1{print $j}' $1 | wc -l)
-    numValues=$(cat $tmpfile | wc -l)
-    #numNonBlank=$(awk -F "\"*,\"*" -v j=$i  'NR>1{print $j}' $1 | sed '/^\s*$/d' | wc -l)
-    numNonBlank=$($tmpfile | sed '/^\s*$/d' | wc -l)
+    printf "Column Name:" &&  awk -v FPAT='[^,]*|"[^"]+"' -v j=$i 'NR==1{print $j}' $1    
+    numValues=$(csvtool col $i $1 | tail -n +2 | wc -l)
+    printf "Number of rows:" && printf $numValues && printf "\n"
+    numNonBlank=$(csvtool col $i $1 | sed '/^\s*$/d' | tail -n +2 | wc -l)
     numMissing=$(echo "$numValues - $numNonBlank" | bc -l)
-    printf "Num Empty/Missing:" 
-    printf $numMissing
+    printf "Num Empty or Missing:"
+    echo $numMissing
     printf "\n"
     printf "Top 3 Values (sorted): " 
-    awk -F "\"*,\"*" -v j=$i  'NR>1{print $j}' $1 | sort | head -n 3 | tr '\n' ' '
+    csvtool col $i $1 | tail -n +2 | sort | head -n 3 | tr '\n' ' '
     printf "\n"
     printf "Bottom 3 Values (sorted): " 
-    awk -F "\"*,\"*" -v j=$i  'NR>1{print $j}' $1 | sort | tail -n 3 | tr '\n' ' '
+    csvtool col $i $1 | tail -n +2 | sort | tail -n 3 | tr '\n' ' '
     printf "\n"
     printf "Most common values (top 5, if that many):\n"
-    awk -v FPAT='[^,]*|"[^"]+"' -v j=$i  'NR>1{print $j}' $1 | sort | uniq -c | sort -n -k1 -r  | head -n 5
+    csvtool col $i $1 | tail -n +2 | sort | uniq -c | sort -n -k1 -r  | head -n 5
     printf "Lead common values (top 5, if that many):\n"
-    awk -F "\"*,\"*" -v j=$i  'NR>1{print $j}' $1 | sort | uniq -c | sort -n -k1 -r  | tail -n 5
+    csvtool col $i $1 | tail -n +2 | sort | uniq -c | sort -n -k1 -r  | tail -n 5
     printf "Summary Stats:\n"
     printf "N \t min \t med \t mean \t max \t stdev\n"
-    (awk -F "\"*,\"*" -v j=$i  'NR>1{print $j}' $1 |
-	 datamash -R 2 count 1 min 1 median 1 mean 1 max 1 sstdev 1 2> /dev/null || printf "Not a number\n")
+    (csvtool col $i $1 | tail -n +2 | datamash -R 2 count 1 min 1 median 1 mean 1 max 1 sstdev 1 2> /dev/null || printf "Not a number\n")
     printf "Summary Stats (missing excluded):\n"
-    printf "min \t med \t mean \t max \t stdev\n"
-    (awk -F "\"*,\"*" -v j=$i  'NR>1{print $j}' $1 | sed '/^\s*$/d' |
-	 datamash -R 2 --no-strict count 1 min 1 median 1 mean 1 max 1 sstdev 1 2> /dev/null || printf "Not a number\n")
+    printf "N \t min \t med \t mean \t max \t stdev\n"
+    (csvtool col $i $1 | tail -n +2 | sed '/^\s*$/d' | datamash -R 2 --no-strict count 1 min 1 median 1 mean 1 max 1 sstdev 1 2> /dev/null || printf "Not a number\n")
     printf "##########################################################"
     printf "\n"
 done
